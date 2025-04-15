c111e159e52b2df0dedcd3337b96a38eb4e29021
David Garcia <dgarc148@ucsc.edu>
2025-04-15 19:27:48 +0000

    updated tokens

--- part2/tokens.py
+++ part2/tokens.py
@@ -1,9 +1,25 @@
 from enum import Enum
-
+import re
 class Token(Enum):
     ID     = "ID"
     NUM    = "NUM"
     IGNORE = "IGNORE"
+    HNUM   = "HNUM"
+    INCR   = "INCR"
+    PLUS   = "PLUS"
+    MULT   = "MULT"
+    SEMI   = "SEMI"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    LBRACE = "LBRACE"
+    RBRACE = "RBRACE"
+    ASSIGN = "ASSIGN"
+    IF     = "IF"
+    ELSE   = "ELSE"
+    WHILE  = "WHILE"
+    INT    = "INT"
+    FLOAT  = "FLOAT"
+
 
 class Lexeme:
     def __init__(self, token:Token, value:str) -> None:
@@ -16,8 +32,31 @@ class Lexeme:
 def idy(l:Lexeme) -> Lexeme:
     return l
 
+keys = {
+    "if": Token.IF,
+    "else": Token.ELSE,
+    "while": Token.WHILE,
+    "int": Token.INT,
+    "float": Token.FLOAT,
+}
+
+def id_or_key(l: Lexeme) -> Lexeme:
+    if l.value in keys:
+        l.token = keys[l.value]
+    return l
+
 tokens = [
-    (Token.ID,     "[a-z]+",  idy),
-    (Token.NUM,    "[0-9]+",  idy),
-    (Token.IGNORE, " |\n",    idy)
+    (Token.ID,     r"[a-zA-Z_][a-zA-Z_0-9]*", id_or_key),
+    (Token.NUM,    r"([0-9]+\.[0-9]+|[0-9]+|\.[0-9]+)", idy),
+    (Token.HNUM,   r"0[xX][0-9a-fA-F]+", idy),
+    (Token.INCR,   r"\+\+", idy),
+    (Token.PLUS,   r"\+", idy),
+    (Token.MULT,   r"\*", idy),
+    (Token.SEMI,   r";", idy),
+    (Token.LPAREN, r"\(", idy),
+    (Token.RPAREN, r"\)", idy),
+    (Token.LBRACE, r"\{", idy),
+    (Token.RBRACE, r"\}", idy),
+    (Token.ASSIGN, r"=", idy),
+    (Token.IGNORE, r"[ \n\t]+", idy)
 ]
